41576a93c0ed120ce929826550e6f2a31b87278c
Paweł Murias <pawelmurias@gmail.com>
2014-02-06 12:42:33 +0100

    Evaling if statements works.

---
+++ Butterfly/Run.hs
@@ -0,0 +1,27 @@
+module Butterfly.Run (eval, emptyHeap, runD, D(..)) where
+import Butterfly.AST
+
+data Heap = Heap
+
+data D = Term Val
+
+emptyHeap = Heap
+
+runD (Term val) = print val
+
+data Val = Integer Integer
+   deriving (Show,Eq)
+
+type Cont = (Val -> Heap -> D)
+
+isTrue :: Val -> Bool
+isTrue (Integer i) = not (i == 0)
+
+eval :: AST -> Heap -> Cont -> D
+
+eval (IntConstant i) heap next = next (Integer i) heap
+eval (If cond then' else') heap next = 
+    eval cond heap (\condVal heap' -> if (isTrue condVal) then eval then' heap' next else eval else' heap' next)
+
+
+
--- butterfly.hs
+++ butterfly.hs
@@ -1,10 +1,11 @@
 import Butterfly.Parser
 import System.Environment
 import Text.ParserCombinators.Parsec
+import Butterfly.Run
 main = do
           [input] <- getArgs
           parsed <- parseFromFile statementlist input
 	  case parsed of
 		Left err -> print err
-		Right x -> print x
+		Right ast -> runD $ eval ast emptyHeap (\val heap -> Term val)
 	   
